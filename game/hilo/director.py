aaa1f02d6e51274db522d963a426a6e34f67c1e9
TomasLopez1 <83098292+TomasLopez1@users.noreply.github.com>
2022-05-10 13:54:48 -0400

    Update director.py
    I addded some comments

--- game/hilo/director.py
+++ game/hilo/director.py
@@ -2,13 +2,28 @@ from game.card import Card
 
 
 class Director:
+'''
+The responsibility of a Director is to control the sequence of play.
+'''
+
 
     def _init_self():
+        
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
 
         self.is_playing = True
         self.points = 0
 
     def start_game():
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
 
 
         while self.is_playing:
@@ -17,7 +32,11 @@ class Director:
             self.do_outputs()
 
     def do_inputs(self):
-        """asks the user whether the card is high or low"""
+        """
+        asks the user whether the card is high or low
+        """
+        
+        
         play_again = input("Do you want to play again? [y/n] ")
         self.is_playing = (play_again == "y")
         print('Playing....')
