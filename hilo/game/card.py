5c61253b0bb2abf91470733314ed4e3db721deab
crosnet <odo21002@byui.edu>
2022-05-11 22:52:42 +0000

    Added some codes to complete the program

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -2,14 +2,23 @@ import random
 
 class Card:
 
-
-
-    def __init__(self):
+    def _init_self():
 
         self.value = 0
-        # self.points = 0
-    
+        self.new_value = 0
+        self.points = 300
+
+
     def draw_card(self):
         self.value = random.randint(1, 13)
-        return self.value
-   
+        self.new_value = random.random(1,13)
+
+        if self.new_value >= self.value:
+            points += 100
+        else:
+            points -= 75
+
+        if self.new_value <= self.value:
+            points += 100
+        else:
+            points -= 75
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -3,27 +3,42 @@ from game.card import Card
 
 class Director:
 
-  def __init__(self):
+    def _init_self():
 
+        self.is_playing = True
+        self.total_score = 300
+
+    def start_game():
+
+
+        while self.is_playing:
+
+            self.show_card()
+            self.do_inputs()
+            self.do_outputs()
+
+    def show_card(self):
+        card1 = self.value
+        print(f"The card is : {card1}")
     
-    self.total_score = 300
-    self.score = 0
-    self.higher_or_lower = ""
+    def get_inputs(self):
+        high_or_low = input("High or low? [h/l]: ")
+        return high_or_low
+    
+    def do_outputs(self):
+        if high_or_low == "h":
+            card2 = self.new_value
+            if card2 >= card1:
+                self.points += self.total_score
+                score = self.points
+            else:
+                self.point -= self.total_score
 
-  def start_game(self):
-
-    while self.total_score > 0:
-
-      self.show_card()
-      self.get_input()
-
-
-  def show_card(self):
-
-    card1 = Card.draw_card(self)
-    print(f"The card is: {card1}")
-
-  def get_input(self):
-
-    self.higher_or_lower = input("Do you want to go higher or lower? [h/l] ") 
-    return self.higher_or_lower   
+        elif high_or_low == "l":
+            card2 = self.new_value
+            if card2 <= card1:
+                self.points += self.total_scores
+            else:
+                self.point -= self.total_score
+                score = self.point
+        print(f"Your score is {score}")
