201449aae5c1baaed5c734f24171d1452f878d94
TomasLopez1 <83098292+TomasLopez1@users.noreply.github.com>
2022-05-11 23:10:25 -0400

    Update card.py

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -10,6 +10,9 @@ class Card:
 
 
     def draw_card(self):
+# Creates a instance to draw a card.
+# Random.randint generates a new random value.
+
         self.value = random.randint(1, 13)
         self.new_value = random.random(1,13)
 
